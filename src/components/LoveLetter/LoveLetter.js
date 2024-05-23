a976f89d8746ff4abf8a21b677e860f62b05566e
MOULALIMS <donmoulali786@gmail.com>
2024-05-23 11:32:38 +0530

    Update LoveLetter component

--- src/components/LoveLetter/LoveLetter.js
+++ src/components/LoveLetter/LoveLetter.js
@@ -35,10 +35,10 @@ const LoveLetter = () => {
       <div className="flap"></div>
       <div className="body"></div>
       <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
-        mah dear kundanapu bomma🖤,<br />
-        never have i been so blessed as to fall in love with someone as wonderful as you...<br />i loveeee youuuu :)<br />
-        always yours,<br />
-        @niihaaarrrr
+        I Love You nana❤️,<br />
+        Since the day we met, Till the day we Talk, End of the Day we worship...<br />Always loving you even in worst phase:)<br />
+        always wanted to be with you ,<br />
+        @Mr. Mike
       </div>
       <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
     </div>
